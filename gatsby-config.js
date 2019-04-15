ce42537a69cf012d446009e1accc58eeab2d9808
Ringo Singson <ringosingson@live.com>
2019-04-15 17:23:38 +0530

    finish upto name

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-google-fonts",
+      options: {
+        fonts: [`PT Sans`, `PT Serif`, `lato`, "expletus sans", `cabin`],
+      },
+    },
     `gatsby-plugin-styled-components`,
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,68 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
+import posed from "react-pose"
 import SocialMedia from "../components/SocialMedia"
+import styled from "styled-components"
+import { greetingOptions } from "../data/configOptions"
 import Layout from "../components/layout"
 import Image from "../components/image"
+import ScrambleText from "../components/ScrambleText"
 import SEO from "../components/seo"
+
+// React-pose configuration that provides a fade on enter transition
+const fadeEnterConfig = {
+  enter: {
+    opacity: 0,
+  },
+  normal: {
+    opacity: 1,
+    transition: { duration: 2500 },
+  },
+}
+
+const MainInfoText = styled(posed.div(fadeEnterConfig))`
+  letter-spacing: 0;
+  font-family: "Expletus Sans", sans-serif;
+  font-weight: bold;
+  font-size: 12vh;
+  text-shadow: 0 0 0;
+  padding-bottom: 20px;
+`
+
+const Greeting = styled(posed.div(fadeEnterConfig))`
+  font-size: 5vh;
+  margin-top: 16%;
+  padding-bottom: 40px;
+`
 
 function IndexPage() {
   const [greeting] = useState(
     greetingOptions[Math.floor(Math.random() * greetingOptions.length)]
   )
-    <h1>Hi people</h1>
-    <SocialMedia />
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  // Load a new random greeting on every page load
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <div>
+        <div>
+          <Greeting>{`${greeting} I'm`}</Greeting>
+        </div>
+        <div>
+          <MainInfoText initialPose="enter" pose="normal">
+            <ScrambleText
+              text="Ringo Singson."
+              scramble="!<>-_\\/[]{}—=+*^?#_abiwxevpi"
+              options={{ duration: 250, speed: 15 }}
+            />
+          </MainInfoText>
+        </div>
+        <SocialMedia />
+      </div>
       <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage

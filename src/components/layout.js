26c3be275587ec24652e7f656791e7d7f0021f89
Ringo Singson <ringosingson@live.com>
2019-04-15 19:43:25 +0530

    adjusting the footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
+import { relative } from "path"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -25,17 +25,25 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
+            position: relative,
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
+            minheight: `100vh`,
           }}
         >
           <main>{children}</main>
-          <footer>
+          <footer
+            style={{
+              position: "absolute",
+              bottom: 0,
+              width: `100%`,
+              height: `2.5rem`,
+            }}
+          >
             © {new Date().getFullYear()}, Built with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>

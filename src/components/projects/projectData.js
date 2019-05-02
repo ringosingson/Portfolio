7dfac2e8f2b27cf2c7e5809fbfde8db4a8e8cc3d
Ringo Singson <ringosingson@live.com>
2019-05-03 02:12:49 +0530

    adding more project

---
+++ src/components/projects/projectData.js
@@ -0,0 +1,97 @@
+export const projectsList = [
+  // eslint-disable-line
+  {
+    name: "Grid-Nexter",
+    desc: "Landing Page",
+    color: "#9CCDA1",
+    imgSource: "/img/projects/project1.png",
+    techStack: [
+      { name: "Html5", icon: "devicons devicons-html5", color: "#3DAEFF" },
+      {
+        name: "CSS3(grid)",
+        icon: "devicons devicons-css3",
+        color: "#0062A8",
+      },
+      {
+        name: "sass",
+        icon: "devicons devicons-sass",
+        color: "#7C2ACF",
+      },
+    ],
+    actionLinks: [
+      {
+        name: "View on Web",
+        icon: "monitor",
+        url: "https://grid-nexter.netlify.com",
+      },
+      {
+        name: "View on Github",
+        icon: "github",
+        url: "https://github.com/ringosingson/grid-nexter",
+      },
+    ],
+  },
+  {
+    name: "Comfy House",
+    desc: "A mockup shop online using vanilla JavaScript",
+    color: "#c873bb",
+    imgSource: "/img/projects/project2.png",
+    techStack: [
+      { name: "Html5", icon: "devicons devicons-html5", color: "#3DAEFF" },
+      {
+        name: "css3(grid & flexbox)",
+        icon: "devicons devicons-css3",
+        color: "#7C2ACF",
+      },
+      {
+        name: "JavaScript",
+        icon: "devicons devicons-javascript",
+        color: "#0062A8",
+      },
+    ],
+    actionLinks: [
+      {
+        name: "View on Web",
+        icon: "monitor",
+        url: "https://stylecom.netlify.com",
+      },
+      {
+        name: "View on Github",
+        icon: "github",
+        url: "https://github.com/ringosingson/stylecom",
+      },
+    ],
+  },
+  {
+    name: "Chat-App",
+    desc: "Simple chat-app with google authentication ",
+    color: "#9CCDA1",
+    imgSource: "/img/projects/project3.png",
+    techStack: [
+      { name: "React", icon: "devicons devicons-atom", color: "#3DAEFF" },
+      {
+        name: "JavaScript",
+        icon: "devicons devicons-javascript",
+        color: "#0062A8",
+      },
+
+      {
+        name: "Firebase",
+        icon: "devicons devicons-codeigniter",
+        color: "#EE8407",
+      },
+    ],
+    actionLinks: [
+      {
+        name: "View on Web",
+        icon: "monitor",
+        url: "https://chat-app-2765a.firebaseapp.com",
+      },
+      {
+        name: "View on Github",
+        icon: "github",
+        url: "https://github.com/ringosingson/chat-app",
+      },
+    ],
+  },
+]
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,16 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
+//import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const ThirdPage = () => (
-  <Layout>
-    <SEO title="Projects" />
-    <h1>Hi from the Projects page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+import styled from "styled-components"
+import { projectsList } from "../components/projects/projectData"
+import ProjectShowcase from "../components/projects/ProjectShowcase"
+//import posed from 'react-pose';
 
-export default ThirdPage
+const ProjectGrid = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 3vmin;
+  justify-items: center;
+`
+
+const Header = styled.div`
+  grid-column: 1 / -1;
+  margin-top: 1em;
+  margin-bottom: 0.5em;
+  font-size: 8vh;
+  font-weight: bold;
+  font-family: "Lato", "Cabin", "Ubuntu", sans-serif;
+`
+
+class ProjectsPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <ProjectGrid>
+          <Header>Featured Projects</Header>
+
+          {projectsList.map((project, i) => (
+            <ProjectShowcase key={i} id={i} project={project} />
+          ))}
+        </ProjectGrid>
+      </Layout>
+    )
+  }
+}
+
+export default ProjectsPage

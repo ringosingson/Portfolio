2f57d99475ea2856fbb1ee4c50e2080bf8eff8b1
Ringo Singson <ringosingson@live.com>
2019-05-04 19:16:19 +0530

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ringo-portfolio-using-gatsby`,
+        short_name: `start`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/portrait_small.jpg`, // This path is relative to the root of the site.
       },
     },
     {
